611c8c7e33f0a8e369ba760ade9b4fa2b289daaa
AmitSutradhar001 <sutradharamit001@gmail.com>
2024-10-01 09:31:02 +0530

    updated

--- content.js
+++ content.js
@@ -1,20 +1,3 @@
-// console.log("goooooooooooooooooo!");
-
-// let h1 = document.getElementsByTagName("h1");
-// for (h of h1) {
-//   h.style["background-color"] = "#FF00FF";
-//   console.log(h.innerHTML);
-// }
-//---------------------------------------------------------------------------------
-// console.log("Fetching all text content...");
-
-// let allText = document.body.innerText; // Get all text content from the page
-// console.log(allText);
-//---------------------------------------------------------------------------------
-// Set up a MutationObserver to detect changes in the DOM
-
-// Working Very Good******
-
 console.log("Fetching all text content...");
 const observer = new MutationObserver((mutations) => {
   mutations.forEach((mutation) => {
@@ -28,12 +11,3 @@ const observer = new MutationObserver((mutations) => {
 
 // Start observing changes to the entire body
 observer.observe(document.body, { childList: true, subtree: true });
-//-----------------------------------------------------------------------------------
-
-// Not Working******
-
-// console.log("Fetching all text content...");
-// let shadowHost = document.querySelector("custom-element"); // Replace with the actual shadow host element
-// let shadowRoot = shadowHost.shadowRoot; // Access the shadow DOM
-// let shadowText = shadowRoot.innerText; // Get text from the shadow DOM
-// console.log(shadowText);
